c68a09635e89134110c980e5018bbabec3c5b757
Big Smarty <budaniasco@gmail.com>
2026-08-25 14:21:40 +0200

    chore: created initial Engine struct and added EngineWrapper value enum

--- src/engine.rs
+++ src/engine.rs
@@ -1,3 +1,57 @@
-pub enum EngineWrapper {}
+use std::marker::PhantomData;
 
-pub struct Engine {}
+#[cfg(feature = "backend-cubecl-cuda")]
+use crate::backend::cubecl_cuda::CubeClCudaBackend;
+#[cfg(feature = "backend-cubecl-hip")]
+use crate::backend::cubecl_hip::CubeClHipBackend;
+#[cfg(feature = "backend-cubecl-vulkan")]
+use crate::backend::cubecl_vulkan::CubeClVulkanBackend;
+#[cfg(feature = "backend-cuda")]
+use crate::backend::native_cuda::NativeCudaBackend;
+#[cfg(feature = "backend-hip")]
+use crate::backend::native_hip::NativeHipBackend;
+#[cfg(feature = "backend-opencl")]
+use crate::backend::native_opencl::NativeOpenClBackend;
+#[cfg(feature = "backend-vulkan")]
+use crate::backend::native_vulkan::NativeVulkanBackend;
+use crate::{backend::Backend, backends::BackendKind};
+
+pub struct Engine<B>
+where
+    B: Backend,
+{
+    device_locator: B::DeviceLocator,
+    context: B::Context,
+    queue: B::Queue,
+    _backend: PhantomData<B>,
+}
+
+impl<B> Engine<B>
+where
+    B: Backend,
+{
+    pub const KIND: BackendKind = B::KIND;
+}
+
+pub enum EngineWrapper {
+    #[cfg(feature = "backend-cuda")]
+    NativeCuda(Engine<NativeCudaBackend>),
+
+    #[cfg(feature = "backend-opencl")]
+    NativeOpenCl(Engine<NativeOpenClBackend>),
+
+    #[cfg(feature = "backend-vulkan")]
+    NativeVulkan(Engine<NativeVulkanBackend>),
+
+    #[cfg(feature = "backend-hip")]
+    NativeHip(Engine<NativeHipBackend>),
+
+    #[cfg(feature = "backend-cubecl-cuda")]
+    CubeClCuda(Engine<CubeClCudaBackend>),
+
+    #[cfg(feature = "backend-cubecl-hip")]
+    CubeClHip(Engine<CubeClHipBackend>),
+
+    #[cfg(feature = "backend-cubecl-vulkan")]
+    CubeClVulkan(Engine<CubeClVulkanBackend>),
+}
