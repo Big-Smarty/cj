e335c2063c2c65ef770e5826e2ffaa54875df7f4
Big Smarty <budaniasco@gmail.com>
2026-08-25 14:38:25 +0200

    chore: added updated trait implementation for backend variants

--- src/backend/native_hip.rs
+++ src/backend/native_hip.rs
@@ -13,27 +13,51 @@ impl Backend for NativeHipBackend {
 
     const KIND: BackendKind = BackendKind::NativeHip;
 
-    fn create_context(&mut self) {
+    type Module = ();
+
+    type InputBuffer = ();
+
+    type OutputBuffer = ();
+
+    fn create_context(locator: &Self::DeviceLocator) -> anyhow::Result<Self::Context> {
         todo!()
     }
 
-    fn create_queue(&mut self) {
+    fn create_queue(context: &Self::Context) -> anyhow::Result<Self::Queue> {
         todo!()
     }
 
-    fn compile_module(&mut self) {
+    fn compile_module(
+        context: &Self::Context,
+        queue: &Self::Queue,
+        kernel: (),
+    ) -> anyhow::Result<Self::Module> {
         todo!()
     }
 
-    fn create_input_buffer(&mut self) {
+    fn create_input_buffer(
+        context: &Self::Context,
+        queue: &Self::Queue,
+        size: usize,
+    ) -> anyhow::Result<Self::InputBuffer> {
         todo!()
     }
 
-    fn create_output_buffer(&mut self) {
+    fn create_output_buffer(
+        context: &Self::Context,
+        queue: &Self::Queue,
+        size: usize,
+    ) -> anyhow::Result<Self::OutputBuffer> {
         todo!()
     }
 
-    fn bench(&mut self) {
+    fn bench(
+        queue: &Self::Queue,
+        module: &Self::Module,
+        input: &Self::InputBuffer,
+        output: &mut Self::OutputBuffer,
+        bench_info: (),
+    ) -> anyhow::Result<()> {
         todo!()
     }
 }
