f2460cfc0b77ead4c023508387f52d712341dc05
Big Smarty <budaniasco@gmail.com>
2026-08-25 17:39:02 +0200

    OpenCL backend implements the new backend trait structure; added initial OpenClSession struct

--- src/backend/native_opencl.rs
+++ src/backend/native_opencl.rs
@@ -1,63 +1,25 @@
-use opencl3::{command_queue::CommandQueue, context::Context, device::Device};
+use opencl3::{command_queue::CommandQueue, context::Context, device::Device, kernel::Kernel};
 
 use crate::{backend::Backend, backends::BackendKind};
 
 pub struct NativeOpenClBackend;
 
 impl Backend for NativeOpenClBackend {
-    type DeviceLocator = Device;
-
-    type Context = Context;
-
-    type Queue = CommandQueue;
+    type Session = OpenClSession;
 
     const KIND: BackendKind = BackendKind::NativeOpenCl;
 
-    type Module = ();
+    type Module = Kernel;
 
     type InputBuffer = ();
 
     type OutputBuffer = ();
+}
 
-    fn create_context(locator: &Self::DeviceLocator) -> anyhow::Result<Self::Context> {
-        todo!()
-    }
+pub struct OpenClSession {
+    context: Context,
+    device: Device,
 
-    fn create_queue(context: &Self::Context) -> anyhow::Result<Self::Queue> {
-        todo!()
-    }
-
-    fn compile_module(
-        context: &Self::Context,
-        queue: &Self::Queue,
-        kernel: (),
-    ) -> anyhow::Result<Self::Module> {
-        todo!()
-    }
-
-    fn create_input_buffer(
-        context: &Self::Context,
-        queue: &Self::Queue,
-        size: usize,
-    ) -> anyhow::Result<Self::InputBuffer> {
-        todo!()
-    }
-
-    fn create_output_buffer(
-        context: &Self::Context,
-        queue: &Self::Queue,
-        size: usize,
-    ) -> anyhow::Result<Self::OutputBuffer> {
-        todo!()
-    }
-
-    fn bench(
-        queue: &Self::Queue,
-        module: &Self::Module,
-        input: &Self::InputBuffer,
-        output: &mut Self::OutputBuffer,
-        bench_info: (),
-    ) -> anyhow::Result<()> {
-        todo!()
-    }
+    // TODO: create the CommandQueue with CL_QUEUE_PROFILING_ENABLE
+    command_queue: CommandQueue,
 }
