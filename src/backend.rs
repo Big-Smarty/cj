e400fa08f7c071ec94d3b9513899705b8efa2c88
Big Smarty <budaniasco@gmail.com>
2026-08-25 14:34:44 +0200

    chore: expanded Backend trait by module type and changed method signatures

--- src/backend.rs
+++ src/backend.rs
@@ -25,13 +25,22 @@ pub trait Backend: Sized + 'static {
     type DeviceLocator;
     type Context;
     type Queue;
+    type Module;
+    type InputBuffer;
+    type OutputBuffer;
 
     const KIND: BackendKind;
 
-    fn create_context(&mut self);
-    fn create_queue(&mut self);
-    fn compile_module(&mut self);
-    fn create_input_buffer(&mut self);
-    fn create_output_buffer(&mut self);
-    fn bench(&mut self);
+    fn create_context(locator: &Self::DeviceLocator);
+    fn create_queue(context: &Self::Context);
+    fn compile_module(context: &Self::Context, queue: &Self::Queue, kernel: ());
+    fn create_input_buffer(context: &Self::Context, queue: &Self::Queue, size: usize);
+    fn create_output_buffer(context: &Self::Context, queue: &Self::Queue, size: usize);
+    fn bench(
+        queue: &Self::Queue,
+        module: &Self::Module,
+        input: &Self::InputBuffer,
+        output: &mut Self::OutputBuffer,
+        bench_info: (),
+    );
 }
