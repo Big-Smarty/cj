d3018b344fb22150196a6e07a1bfafb8da85a0cb
Big Smarty <budaniasco@gmail.com>
2026-08-25 17:18:36 +0200

    reorganizing backend trait

--- src/backend.rs
+++ src/backend.rs
@@ -16,37 +16,10 @@ pub mod native_opencl;
 //pub mod cubecl_hip;
 
 pub trait Backend: Sized + 'static {
-    type DeviceLocator;
-    type Context;
-    type Queue;
+    type Session;
     type Module;
     type InputBuffer;
     type OutputBuffer;
 
     const KIND: BackendKind;
-
-    fn create_context(locator: &Self::DeviceLocator) -> anyhow::Result<Self::Context>;
-    fn create_queue(context: &Self::Context) -> anyhow::Result<Self::Queue>;
-    fn compile_module(
-        context: &Self::Context,
-        queue: &Self::Queue,
-        kernel: (),
-    ) -> anyhow::Result<Self::Module>;
-    fn create_input_buffer(
-        context: &Self::Context,
-        queue: &Self::Queue,
-        size: usize,
-    ) -> anyhow::Result<Self::InputBuffer>;
-    fn create_output_buffer(
-        context: &Self::Context,
-        queue: &Self::Queue,
-        size: usize,
-    ) -> anyhow::Result<Self::OutputBuffer>;
-    fn bench(
-        queue: &Self::Queue,
-        module: &Self::Module,
-        input: &Self::InputBuffer,
-        output: &mut Self::OutputBuffer,
-        bench_info: (),
-    ) -> anyhow::Result<()>;
 }
